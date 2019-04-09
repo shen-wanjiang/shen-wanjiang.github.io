5bc4e7b8b674d1a3be59d89e0e5c1855630c70a7
shen <shenwanjiang@vargo.com.cn>
2019-04-09 15:02:36 +0800

    update

---
+++ img/comment.rb
@@ -0,0 +1,28 @@
+username = "shen-wanjiang" # GitHub 用户名
+new_token = "49bf1498b4e55bdb9c83698972d7ff53cb64b261"  # GitHub Token
+repo_name = "github-comments-repo" # 存放 issues
+sitemap_url = "https://draveness.me/sitemap.xml" # sitemap
+kind = "Gitalk" # "Gitalk" or "gitment"
+
+require 'open-uri'
+require 'faraday'
+require 'active_support'
+require 'active_support/core_ext'
+require 'sitemap-parser'
+
+sitemap = SitemapParser.new sitemap_url
+urls = sitemap.to_a
+
+conn = Faraday.new(:url => "https://api.github.com/repos/#{username}/#{repo_name}/issues") do |conn|
+  conn.basic_auth(username, new_token)
+  conn.adapter  Faraday.default_adapter
+end
+
+urls.each_with_index do |url, index|
+  title = open(url).read.scan(/<title>(.*?)<\/title>/).first.first.force_encoding('UTF-8')
+  response = conn.post do |req|
+    req.body = { body: url, labels: [kind, url], title: title }.to_json
+  end
+  puts response.body
+  sleep 15 if index % 20 == 0
+end
